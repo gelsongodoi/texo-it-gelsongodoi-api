fe3126db04333871bddc2fb98f2d1071a38e07a7
Gelson <gelson.godoi@gmail.com>
2024-08-29 01:45:09 -0300

    ajuste no arquivo de propriedades

--- src/main/java/br/com/texoit/movie/util/MovieCSVLoader.java
+++ src/main/java/br/com/texoit/movie/util/MovieCSVLoader.java
@@ -1,7 +1,6 @@
 package br.com.texoit.movie.util;
 
 import br.com.texoit.movie.dto.MovieRequestDto;
-import br.com.texoit.movie.exception.ResourceNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
